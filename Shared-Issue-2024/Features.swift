2abcb9bb73593527271e48475060d9f544ee5f92
Dominik Mayer <dominik.mayer@gmail.com>
2024-10-25 17:41:31 +0700

    Initial setup

---
+++ Shared-Issue-2024/Features.swift
@@ -0,0 +1,85 @@
+//
+//  Features.swift
+//  Shared-Issue-2024
+//
+//  Created by Dominik Mayer on 25.10.24.
+//
+
+import ComposableArchitecture
+import Foundation
+
+extension PersistenceReaderKey where Self == PersistenceKeyDefault<InMemoryKey<Journal?>> {
+    static var journal: Self {
+        return PersistenceKeyDefault(.inMemory("journal"), nil)
+    }
+}
+
+@Reducer
+struct EntryFeature: Sendable {
+    
+    @ObservableState
+    struct State: Equatable, Identifiable, Sendable {
+        var id: URL {
+            entry.id
+        }
+        
+        @Shared var entry: Entry
+        @SharedReader(.journal) var journal
+    }
+}
+
+@Reducer
+struct DayFeature {
+    
+    @ObservableState
+    struct State: Equatable {
+        var entries: [EntryFeature.State]
+    }
+}
+
+@Reducer
+struct CalendarFeature {
+    
+    @Reducer(state: .equatable)
+    enum Destination {
+        case day(DayFeature)
+        case entry(EntryFeature)
+    }
+    
+    @ObservableState
+    struct State: Equatable {
+
+        @Presents
+        var destination: Destination.State?
+        @Shared(.journal) var journal
+    }
+    
+    enum Action: Equatable {
+        case destination(PresentationAction<Destination.Action>)
+        case loadEntry(URL)
+    }
+    
+    var body: some ReducerOf<Self> {
+        Reduce { state, action in
+            switch action {
+                
+            case .loadEntry(let url):
+                guard let journal = Shared(state.$journal),
+                      let entry = Shared(journal[url])
+                else {
+                    return .none
+                }
+
+                let updatedEntryState = EntryFeature.State(entry: entry)
+                state.destination = .entry(updatedEntryState)
+                return .none
+                
+            case .destination:
+                return .none
+            }
+        }
+        .ifLet(\.$destination, action: \.destination)
+    }
+}
+
+extension CalendarFeature.Destination.Action: Equatable {}
--- Shared-Issue-2024Tests/Shared_Issue_2024Tests.swift
+++ Shared-Issue-2024Tests/Shared_Issue_2024Tests.swift
@@ -5,12 +5,79 @@
 //  Created by Dominik Mayer on 25.10.24.
 //
 
+import ComposableArchitecture
+import Foundation
 import Testing
 
-struct Shared_Issue_2024Tests {
+@Suite("CalendarFeature", .serialized)
+@MainActor
+struct Test_CalendarFeature {
 
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    let journal = Journal(entries: [.entry1, .entry2, .entry3])
+        
+    @Test
+    func settingSharedVariableNil() async {
+        @Shared(.journal) var sharedJournal
+        
+        // We never use this store but if we remove it, the issue doesn't appear
+        let _ = TestStore(
+            initialState: .init(
+                destination: .day(.init(entries: [.entry1Empty]))
+                // This has to be the same entry that will be set by the other test. You can try to change it to .entry2Empty and everything passes
+            )
+        ) {
+            CalendarFeature()
+        }
+
+        $sharedJournal.withLock {
+            $0 = nil
+        }
+        // We can but don't even need to send an action here
     }
+    
+    // This test passes when it's run alone but fails when it's run as part of the suite together with the other test, even when both tests are serialized
+    @Test
+    func brittleTest() async {
+        @Shared(.journal) var sharedJournal
+        $sharedJournal.withLock {
+            $0 = journal
+        }
 
+        let store = TestStore(
+            initialState: .init()
+        ) {
+            CalendarFeature()
+        }
+        
+        await store.send(.loadEntry(.entry1)) {
+            $0.destination = .entry(.entry1Empty)
+        }
+    }
+}
+
+extension URL {
+    static let entry1 = URL(fileURLWithPath: "/some/path/")
+    static let entry2 = URL(fileURLWithPath: "/some/other/path/")
+    static let entry3 = URL(fileURLWithPath: "/some/third/path/")
+}
+
+extension Entry {
+    static let entry1 = Entry(
+        url: .entry1
+    )
+    static let entry2 = Entry(
+        url: .entry2
+    )
+    static let entry3 = Entry(
+        url: .entry3
+    )
+}
+
+extension EntryFeature.State {
+    static let entry1Empty = EntryFeature.State(
+        entry: Shared(.entry1)
+    )
+    static let entry2Empty = EntryFeature.State(
+        entry: Shared(.entry2)
+    )
 }
